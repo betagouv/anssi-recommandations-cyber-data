a9766457ad626002b0c736d8c0cf577a94910890
pie-leroy <pierre.leroy@beta.gouv.fr>
2025-10-08 17:34:15 +0200

    [SOIN] Refactorise le fichier main d'evalap

--- src/main_evalap.py
+++ src/main_evalap.py
@@ -1,11 +1,58 @@
 import pandas as pd
 from pathlib import Path
 from argparse import ArgumentParser
-from src.evalap.evalap_dataset_http import DatasetPayload
-from src.configuration import Evalap
+from src.evalap.evalap_dataset_http import DatasetPayload, DatasetReponse
+from src.evalap.evalap_experience_http import ExperiencePayload
 from src.evalap import EvalapClient
-from src.configuration import recupere_configuration
+from src.configuration import recupere_configuration, Configuration
 import requests
+from typing import Optional
+
+
+def ajoute_dataset(
+    client: EvalapClient, nom: str, df_mapped: pd.DataFrame
+) -> Optional[DatasetReponse]:
+    payload = DatasetPayload(
+        name=nom,
+        readme="Jeu d'évaluation QA pour Evalap",
+        default_metric="judge_precision",
+        df=df_mapped.astype(object).where(pd.notnull(df_mapped), None).to_json(),
+    )
+
+    resultat = client.dataset.ajoute(payload)
+
+    if resultat is None:
+        print("Le dataset n'a pas pu être ajouté")
+        return None
+
+    print("Dataset ajouté :")
+    print(client.dataset.liste())
+    return resultat
+
+
+def cree_experience(
+    client: EvalapClient,
+    dataset: DatasetReponse,
+    df_mapped: pd.DataFrame,
+    conf: Configuration,
+):
+    payload_experience = ExperiencePayload(
+        name="Experience Test",
+        metrics=["judge_precision"],
+        dataset=dataset.name,
+        model={
+            "output": df_mapped["output"].astype(str).tolist(),
+            "aliased_name": "precomputed",
+        },
+        judge_model={
+            "name": "albert-large",
+            "base_url": conf.albert.url,
+            "api_key": conf.albert.cle_api,
+        },
+    )
+
+    resultat_experience = client.experience.ajoute(payload_experience)
+    print(resultat_experience)
 
 
 def main():
@@ -13,6 +60,10 @@ def main():
     p.add_argument("--csv", required=True, type=Path, help="Chemin du CSV d'entrée")
     p.add_argument("--nom", required=True, type=str, help="Nom du dataset")
     args = p.parse_args()
+
+    if not args.csv.exists():
+        print(f"Le fichier {args.csv} n'existe pas")
+        return
 
     conf = recupere_configuration()
     df = pd.read_csv(args.csv)
@@ -24,20 +75,14 @@ def main():
     }
 
     df_mapped = df.rename(columns=columns_map)
-
-    payload = DatasetPayload(
-        name=args.nom,
-        readme="Jeu d'évaluation QA pour Evalap",
-        default_metric="judge_precision",
-        df=df_mapped.astype(object).where(pd.notnull(df_mapped), None).to_json(),
-    )
     session = requests.Session()
-
     client = EvalapClient(conf, session=session)
-    _resultat = client.dataset.ajoute(payload)
 
-    print("Dataset ajouté :")
-    print(client.dataset.liste())
+    dataset = ajoute_dataset(client, args.nom, df_mapped)
+    if dataset is None:
+        return
+
+    cree_experience(client, dataset, df_mapped, conf)
 
 
 if __name__ == "__main__":
