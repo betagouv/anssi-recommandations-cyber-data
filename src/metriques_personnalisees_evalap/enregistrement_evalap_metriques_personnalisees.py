43f9a898364479dd1fe751d52dc66090205402f5
pie-leroy <pierre.leroy@beta.gouv.fr>
2025-10-30 16:15:39 +0100

    [SOIN] Nettoie `metriques_personnalisees.py`
    * Ajoute du typage
    * Supprime et déplace dans `enregistrement_evalap_metriques_personnalisees.py` le troisième argument retourné jusque la, qui est un dictionnaire vide nécessaire au format d'Evalap.

--- src/metriques_personnalisees_evalap/enregistrement_evalap_metriques_personnalisees.py
+++ src/metriques_personnalisees_evalap/enregistrement_evalap_metriques_personnalisees.py
@@ -27,7 +27,8 @@ def _enregistre_metriques_par_position(
         def metrique(output, output_true, **kwargs):
             param_verite_value = kwargs.get(param_verite, valeur_defaut)
             param_bot_value = kwargs.get(f"{param_bot}_{position}", valeur_defaut)
-            return fonction_metrique(param_bot_value, param_verite_value)
+            score, observation = fonction_metrique(param_bot_value, param_verite_value)
+            return score, observation, {}
 
         return metrique
 
