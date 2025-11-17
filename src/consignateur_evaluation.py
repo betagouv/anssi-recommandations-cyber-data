8e17b92fb03f5588511c987342d6d60e1ff6802e
pie-leroy <pierre.leroy@beta.gouv.fr>
2025-11-17 11:31:57 +0100

    Modifie consigne_evaluation pour prendre en paramètre un dataframe ou des bytes

--- src/consignateur_evaluation.py
+++ src/consignateur_evaluation.py
@@ -1,13 +1,30 @@
+import io
 from argparse import ArgumentParser
 from pathlib import Path
+from typing import Union
 
 import pandas as pd
 
-from adaptateurs.journal import AdaptateurJournal, Donnees, TypeEvenement, fabrique_adaptateur_journal
+from adaptateurs.journal import (
+    AdaptateurJournal,
+    Donnees,
+    TypeEvenement,
+    fabrique_adaptateur_journal,
+)
 
 
-def consigne_evaluation(df: pd.DataFrame, adaptateur_journal: AdaptateurJournal):
-    contenu_evaluation = df.to_dict("records")
+def consigne_evaluation(
+    resultats_metriques: Union[pd.DataFrame, bytes],
+    adaptateur_journal: AdaptateurJournal,
+):
+    match resultats_metriques:
+        case pd.DataFrame():
+            contenu_evaluation = resultats_metriques.to_dict("records")
+        case bytes():
+            contenu_evaluation = pd.read_pickle(
+                io.BytesIO(resultats_metriques)
+            ).to_dict("records")
+
     for evaluation in contenu_evaluation:
         donnees_evaluation = Donnees.model_validate(evaluation)
         adaptateur_journal.consigne_evenement(
@@ -15,13 +32,18 @@ def consigne_evaluation(df: pd.DataFrame, adaptateur_journal: AdaptateurJournal)
             donnees_evaluation,
         )
 
+
 def main() -> None:
     p = ArgumentParser(description="Remplir 'Réponse Bot' depuis 'Question'")
     p.add_argument("--csv", required=True, type=Path, help="Chemin du CSV d'entrée")
     args = p.parse_args()
     df_evaluation = pd.read_csv(args.csv)
 
-    consigne_evaluation(df=df_evaluation, adaptateur_journal=fabrique_adaptateur_journal())
+    consigne_evaluation(
+        resultats_metriques=df_evaluation,
+        adaptateur_journal=fabrique_adaptateur_journal(),
+    )
+
 
 if __name__ == "__main__":
     main()
--- tests/test_ecriture_metabase.py
+++ tests/test_ecriture_metabase.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from adaptateurs.journal import TypeEvenement, AdaptateurJournalMemoire
 from consignateur_evaluation import consigne_evaluation
+import io
 
 
 def test_consigne_evenement_suite_recuperation_fichier_sortie_evaluation_evalap():
@@ -80,4 +81,40 @@ def test_consigne_les_evenements_extrait_dynamiquement_le_nom_des_colonnes():
         "id_experimentation": 1,
         "score_numero_page_en_contexte_4": 0.7,
         "nouvelle_metrique": 0,
+    }
+
+
+def test_consigne_evenement_suite_recuperation_fichier_sortie_evaluation_evalap_au_format_byte():
+    adaptateur_journal: AdaptateurJournalMemoire = AdaptateurJournalMemoire()
+    df = pd.DataFrame(
+        [
+            {
+                "id_experimentation": 1,
+                "score_numero_page_en_contexte_4": 0.6,
+                "bon_nom_document_en_contexte_2": 1,
+            },
+            {
+                "id_experimentation": 1,
+                "score_numero_page_en_contexte_4": 0.7,
+                "bon_nom_document_en_contexte_2": 0,
+            },
+        ]
+    )
+    tampon = io.BytesIO()
+    df.to_pickle(tampon)
+    donnees_byte = tampon.getvalue()
+
+    consigne_evaluation(donnees_byte, adaptateur_journal)
+
+    les_evenements = adaptateur_journal.les_evenements()
+    assert len(les_evenements) == 2
+    assert les_evenements[0]["donnees"] == {
+        "id_experimentation": 1,
+        "score_numero_page_en_contexte_4": 0.6,
+        "bon_nom_document_en_contexte_2": 1,
+    }
+    assert les_evenements[1]["donnees"] == {
+        "id_experimentation": 1,
+        "score_numero_page_en_contexte_4": 0.7,
+        "bon_nom_document_en_contexte_2": 0,
     }
