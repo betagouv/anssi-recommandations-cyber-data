cb2b7e56813e259e7c2dfa9687168a175b91a76b
pie-leroy <pierre.leroy@beta.gouv.fr>
2025-11-18 16:22:47 +0100

    [SOIN] Extrais un exemple de donnees pour les tests d'experience Evalap

--- tests/test_experience.py
+++ tests/test_experience.py
@@ -4,6 +4,7 @@ from requests import Response
 from configuration import Configuration
 from evalap import EvalapClient
 from journalisation.experience import EntrepotExperienceHttp, Experience
+from test_client_evalap import DONNEES_JSON
 
 
 def test_peut_lire_une_experience(
@@ -11,65 +12,27 @@ def test_peut_lire_une_experience(
 ):
     session = Mock()
 
-    donnees_json = {
-        "id": 42,
-        "name": "Experience Test",
-        "created_at": "2025-10-06T15:45:00Z",
-        "experiment_status": "running_metrics",
-        "experiment_set_id": 1,
-        "num_try": 8,
-        "num_success": 7,
-        "num_observation_try": 40,
-        "num_observation_success": 38,
-        "num_metrics": 3,
-        "readme": "Test readme",
-        "judge_model": {"model": "albert"},
-        "model": {"name": "albert-large"},
-        "dataset": {"id": 10},
-        "with_vision": False,
-        "results": [
+    donnees_json: dict = DONNEES_JSON
+    donnees_json["results"].append({
+        "created_at": "2025-10-09T14:48:35.428847",
+        "experiment_id": 42,
+        "id": 125,
+        "metric_name": "autre_metrique",
+        "metric_status": "running",
+        "num_success": 0,
+        "num_try": 0,
+        "observation_table": [
             {
+                "id": 1002,
                 "created_at": "2025-10-09T14:48:35.428847",
-                "experiment_id": 42,
-                "id": 125,
-                "metric_name": "judge_precision",
-                "metric_status": "running",
-                "num_success": 0,
-                "num_try": 0,
-                "observation_table": [
-                    {
-                        "id": 1001,
-                        "created_at": "2025-10-09T14:48:35.428847",
-                        "score": 0.8,
-                        "observation": "test",
-                        "num_line": 0,
-                        "error_msg": None,
-                        "execution_time": 5,
-                    }
-                ],
-            },
-            {
-                "created_at": "2025-10-09T14:48:35.428847",
-                "experiment_id": 42,
-                "id": 125,
-                "metric_name": "autre_metrique",
-                "metric_status": "running",
-                "num_success": 0,
-                "num_try": 0,
-                "observation_table": [
-                    {
-                        "id": 1002,
-                        "created_at": "2025-10-09T14:48:35.428847",
-                        "score": 0.4,
-                        "observation": "test",
-                        "num_line": 0,
-                        "error_msg": None,
-                        "execution_time": 5,
-                    }
-                ],
-            },
+                "score": 0.4,
+                "observation": "test",
+                "num_line": 0,
+                "error_msg": None,
+                "execution_time": 5,
+            }
         ],
-    }
+    },)
 
     reponse_mockee = Mock(spec=Response)
     reponse_mockee.status_code = 200
