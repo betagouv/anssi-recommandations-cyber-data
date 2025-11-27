440911e2565b160f34039265e992721f632409e0
pie-leroy <pierre.leroy@beta.gouv.fr>
2025-11-27 16:58:45 +0100

    Ajoute la métrique personnalisée bon nom document
    Ajoute les metriques bon nom documents

--- tests/deepeval/test_metriques_personalisees.py
+++ tests/deepeval/test_metriques_personalisees.py
@@ -1,5 +1,10 @@
 from deepeval.test_case import LLMTestCase
-from evaluation.metriques_personnalisees.metrique_longueur_reponse import (
+
+from evaluation.metriques_personnalisees.de_deepeval.metrique_bon_nom_document import (
+    MetriqueBonNomDocument,
+    MetriquesBonNomDocuments,
+)
+from evaluation.metriques_personnalisees.de_deepeval.metrique_longueur_reponse import (
     MetriqueLongueurReponse,
 )
 
@@ -33,3 +38,68 @@ def test_la_metrique_qui_mesure_la_longeur_de_la_reponse_renvoie_0_si_pas_de_rep
 
     assert score == 0
     assert metrique.is_successful() is False
+
+
+def test_metrique_bon_nom_document_en_contexte_retourne_1_si_meme_document():
+    metadatas = {
+        "nom_document_reponse_bot_0": "document_test",
+        "nom_document_verite_terrain_0": "document_test",
+    }
+
+    cas_test = LLMTestCase(
+        input="Question test", actual_output="", additional_metadata=metadatas
+    )
+    metrique = MetriqueBonNomDocument(
+        nom_document_reponse="nom_document_reponse_bot_0",
+        nom_document_verite_terrain="nom_document_verite_terrain_0",
+    )
+    score = metrique.measure(cas_test)
+
+    assert score == 1
+    assert metrique.is_successful() is True
+
+
+def test_metrique_bon_nom_document_en_contexte_retourne_0_si_document_different():
+    metadatas = {
+        "nom_document_reponse_bot_0": "document_test",
+        "nom_document_verite_terrain_0": "document_autre",
+    }
+
+    cas_test = LLMTestCase(
+        input="Question test", actual_output="", additional_metadata=metadatas
+    )
+    metrique = MetriqueBonNomDocument(
+        nom_document_reponse="nom_document_reponse_bot_0",
+        nom_document_verite_terrain="nom_document_verite_terrain_0",
+    )
+    score = metrique.measure(cas_test)
+
+    assert score == 0
+    assert metrique.is_successful() is False
+
+
+def test_metriques_bon_nom_documents_en_contexte_retourne_1_si_meme_document():
+    metadatas = {
+        "nom_document_reponse_bot_0": "document_test",
+        "nom_document_verite_terrain_0": "document_test",
+        "nom_document_reponse_bot_1": "document_test1",
+        "nom_document_verite_terrain_1": "document_test1",
+        "nom_document_reponse_bot_2": "document_test2",
+        "nom_document_verite_terrain_2": "document_test2",
+        "nom_document_reponse_bot_3": "document_test3",
+        "nom_document_verite_terrain_3": "document_test3",
+        "nom_document_reponse_bot_4": "document_test4",
+        "nom_document_verite_terrain_4": "document_test4",
+    }
+
+    cas_test = LLMTestCase(
+        input="Question test", actual_output="", additional_metadata=metadatas
+    )
+    metriques = MetriquesBonNomDocuments.cree_metriques()
+
+    assert len(metriques) == 5
+    assert metriques[0].measure(cas_test) == 1
+    assert metriques[1].measure(cas_test) == 1
+    assert metriques[2].measure(cas_test) == 1
+    assert metriques[3].measure(cas_test) == 1
+    assert metriques[4].measure(cas_test) == 1
