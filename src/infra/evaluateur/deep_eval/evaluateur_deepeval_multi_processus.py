2c1a4c2197ec3b62d6f41818d962899b6bb1dc35
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-11-30 21:48:33 +0100

    Implémente un évaluateur deepeval multi processus

---
+++ src/infra/evaluateur/deep_eval/evaluateur_deepeval_multi_processus.py
@@ -0,0 +1,42 @@
+import multiprocessing as mp
+from itertools import islice
+from typing import Optional, Generator
+from deepeval.evaluate import evaluate
+from deepeval.evaluate.types import EvaluationResult
+from deepeval.metrics import BaseMetric
+from deepeval.test_case import LLMTestCase
+
+from evaluation.lanceur_deepeval import EvaluateurDeepeval
+
+
+class EvaluateurDeepevalMultiProcessus(EvaluateurDeepeval):
+    def __init__(self, nb_processus=1, taille_lot=1):
+        self.taille_lot = taille_lot
+        self.nb_processus = nb_processus
+        self.metrics = []
+
+    def evaluate(
+        self, test_cases: list[LLMTestCase], metrics: Optional[list[BaseMetric]] = None
+    ) -> list[EvaluationResult]:
+        self.metrics = metrics
+        lots = self.divise_en_lots(test_cases)
+        with mp.Pool(processes=self.nb_processus) as pool:
+            resultats = pool.map(self.execute_evaluation, lots)
+
+        return resultats
+
+    def execute_evaluation(
+        self, les_cas_de_test: list[LLMTestCase]
+    ) -> EvaluationResult:
+        return evaluate(les_cas_de_test, self.metrics)
+
+    def divise_en_lots(self, cas_de_test: list[LLMTestCase]) -> list[list[LLMTestCase]]:
+        return list(self.genere_chunk(cas_de_test))
+
+    def genere_chunk(self, iterable: list[LLMTestCase])-> Generator[list[LLMTestCase]]:
+        it = iter(iterable)
+        while True:
+            chunk = list(islice(it, self.nb_processus))
+            if not chunk:
+                break
+            yield chunk
