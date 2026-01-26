e0a17c2f56bfb91753a3ca118bd2f1107677bf59
unknown <pie.leroy@hotmail.com>
2026-01-26 21:02:17 +0100

    Modifie les chemins et la sortie pour jouer les tests "CROSS OS"

--- src/infra/evaluateur/deep_eval/evaluateur_deepeval_multi_processus.py
+++ src/infra/evaluateur/deep_eval/evaluateur_deepeval_multi_processus.py
@@ -1,12 +1,20 @@
 import multiprocessing as mp
+import sys
 from itertools import islice
 from typing import Optional, Generator
-from deepeval.evaluate import evaluate
+from deepeval.evaluate import evaluate, DisplayConfig
 from deepeval.evaluate.types import EvaluationResult
 from deepeval.metrics import BaseMetric
 from deepeval.test_case import LLMTestCase
 
 from evaluation.lanceur_deepeval import EvaluateurDeepeval
+
+
+def configure_sorties_utf8():
+    for flux in (sys.stdout, sys.stderr):
+        reconfigure = getattr(flux, "reconfigure", None)
+        if callable(reconfigure):
+            reconfigure(encoding="utf-8", errors="replace")
 
 
 class EvaluateurDeepevalMultiProcessus(EvaluateurDeepeval):
@@ -19,7 +27,9 @@ class EvaluateurDeepevalMultiProcessus(EvaluateurDeepeval):
     ) -> list[EvaluationResult]:
         self.metrics = metrics
         lots = self.divise_en_lots(test_cases)
-        with mp.Pool(processes=self.nb_processus) as pool:
+        with mp.Pool(
+            processes=self.nb_processus, initializer=configure_sorties_utf8
+        ) as pool:
             resultats = pool.map(self.execute_evaluation, lots)
 
         return resultats
@@ -27,7 +37,15 @@ class EvaluateurDeepevalMultiProcessus(EvaluateurDeepeval):
     def execute_evaluation(
         self, les_cas_de_test: list[LLMTestCase]
     ) -> EvaluationResult:
-        return evaluate(les_cas_de_test, self.metrics)
+        return evaluate(
+            les_cas_de_test,
+            self.metrics,
+            display_config=DisplayConfig(
+                print_results=False,
+                show_indicator=False,
+                verbose_mode=False,
+            ),
+        )
 
     def divise_en_lots(self, cas_de_test: list[LLMTestCase]) -> list[list[LLMTestCase]]:
         return list(self.genere_chunk(cas_de_test))
