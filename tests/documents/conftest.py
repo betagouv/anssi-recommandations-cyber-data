350685ee14ff30653ec7b5d3eeb8fda11836f5b2
Bertrand Bougon <bertrand.bougon@gmail.com>
2026-03-03 09:01:23 +0100

    [SOIN] Supprime les fixtures et constructeurs inutiles

--- tests/documents/conftest.py
+++ tests/documents/conftest.py
@@ -2,7 +2,6 @@ import json
 import sys
 from pathlib import Path
 from typing import Callable, Type, Union, Optional
-from unittest.mock import Mock
 
 import pytest
 from docling.backend.pdf_backend import PdfDocumentBackend
@@ -11,7 +10,6 @@ from docling.datamodel.document import InputDocument
 from docling.datamodel.settings import PageRange
 from docling.document_converter import ConversionResult
 from docling.document_converter import DocumentConverter
-from docling_core.transforms.chunker import BaseChunk, DocMeta
 from docling_core.types import DoclingDocument
 from docling_core.types.doc import (
     BoundingBox,
@@ -19,13 +17,11 @@ from docling_core.types.doc import (
     TextItem,
     DocItemLabel,
     SectionHeaderItem,
-    DocItem,
     TableItem,
     TableData,
     TableCell,
 )
 from docling_core.types.io import DocumentStream
-from requests import Session
 
 from documents.chunker_docling import TypeFichier
 from documents.chunker_docling_mqc import ChunkerDoclingMQC
@@ -64,16 +60,6 @@ def fichier_urls_specifiques(tmp_path) -> Path:
     chemin_fichier.write_text(json.dumps(contenu), encoding="utf-8")
 
     return chemin_fichier
-
-
-@pytest.fixture
-def mock_post_session_creation_document() -> Callable[[Session, ReponseDocument], None]:
-    def _mock(session: Session, reponse_attendue: ReponseDocument) -> None:
-        mock_response = Mock()
-        mock_response.json.return_value = reponse_attendue._asdict()
-        session.post = Mock(return_value=mock_response)  # type: ignore[method-assign]
-
-    return _mock
 
 
 @pytest.fixture
@@ -333,158 +319,6 @@ def un_convertisseur_avec_deux_textes_dont_un_sans_bbox() -> Callable[
 
 
 @pytest.fixture
-def un_convertisseur_se_basant_sur_un_guide_anssi() -> Callable[
-    [], Type[DocumentConverter]
-]:
-    class ConverterDeTestAvecBbox(DocumentConverter):
-        def convert(
-            self,
-            document: Union[Path, str, DocumentStream],  # TODO review naming
-            headers: Optional[dict[str, str]] = None,
-            raises_on_error: bool = True,
-            max_num_pages: int = sys.maxsize,
-            max_file_size: int = sys.maxsize,
-            page_range: PageRange = (1, 1),
-        ) -> ConversionResult:
-            numero_page = 11
-            premier_paragraphe = (
-                ConstructeurDeTextItem()
-                .avec_numero_page(numero_page)
-                .avec_texte(
-                    "afin d’accélérer leur recherche, puis de comparer "
-                    "automatiquement chacune d’entre elles à une empreinte "
-                    "volée jusqu’à trouver une égalité qui prouvera que le "
-                    "mot de passe recherché a été trouvé."
-                )
-                .avec_bbox(BoundingBox(l=86.0, t=767.0, r=536.0, b=797.0))
-                .construis()
-            )
-            deuxieme_paragraphe = (
-                ConstructeurDeTextItem()
-                .avec_numero_page(numero_page)
-                .avec_texte(
-                    "Attaque par ingénierie sociale. Cette "
-                    "attaque consiste à récupérer un mot "
-                    "de passe par des moyens détournés "
-                    "comme l’hameçonnage ou bien l’usurpation "
-                    "d’identité."
-                )
-                .avec_bbox(BoundingBox(l=73.0, t=716.0, r=536.0, b=740.0))
-                .construis()
-            )
-            premiere_entete = (
-                ConstructeurDeTextItem()
-                .avec_numero_page(numero_page)
-                .avec_texte("Menace")
-                .avec_bbox(BoundingBox(l=78.0, t=620.0, r=117.0, b=627.0))
-                .construis()
-            )
-            premiere_ligne_colonne_1 = (
-                ConstructeurDeTextItem()
-                .avec_numero_page(numero_page)
-                .avec_texte("Recherche exhaustive")
-                .avec_bbox(BoundingBox(l=78.0, t=580.0, r=181.0, b=588.0))
-                .construis()
-            )
-            deuxieme_ligne_colonne_1 = (
-                ConstructeurDeTextItem()
-                .avec_numero_page(numero_page)
-                .avec_texte("Recherche par dictionnaire")
-                .avec_bbox(BoundingBox(l=78.0, t=536.0, r=207.0, b=546.0))
-                .construis()
-            )
-            deuxieme_entete = (
-                ConstructeurDeTextItem()
-                .avec_numero_page(numero_page)
-                .avec_texte("Contre-mesures")
-                .avec_bbox(BoundingBox(l=317.0, t=620.0, r=395.0, b=627.0))
-                .construis()
-            )
-            premiere_ligne_colonne_2 = (
-                ConstructeurDeTextItem()
-                .avec_numero_page(numero_page)
-                .avec_texte("Limite temporelle entre chaque essai (contre les")
-                .avec_bbox(BoundingBox(l=317.0, t=601.0, r=543.0, b=611.0))
-                .construis()
-            )
-            deuxieme_ligne_colonne_2 = (
-                ConstructeurDeTextItem()
-                .avec_numero_page(numero_page)
-                .avec_texte("attaquants en ligne) et fonctions de hachage ité-")
-                .avec_bbox(BoundingBox(l=317.0, t=587.0, r=543.0, b=597.0))
-                .construis()
-            )
-            troisieme_ligne_colonne_2 = (
-                ConstructeurDeTextItem()
-                .avec_numero_page(numero_page)
-                .avec_texte("ratives dédiées (contre les attaquants en ligne et")
-                .avec_bbox(BoundingBox(l=317.0, t=573.0, r=543.0, b=583.0))
-                .construis()
-            )
-            quatrieme_ligne_colonne_2 = (
-                ConstructeurDeTextItem()
-                .avec_numero_page(numero_page)
-                .avec_texte("hors ligne)")
-                .avec_bbox(BoundingBox(l=317.0, t=558.0, r=367.0, b=568.0))
-                .construis()
-            )
-            cinquieme_ligne_colonne_2 = (
-                ConstructeurDeTextItem()
-                .avec_numero_page(numero_page)
-                .avec_texte("Mots de passe robustes et aléatoires et coffre-")
-                .avec_bbox(BoundingBox(l=317.0, t=544.0, r=543.0, b=554.0))
-                .construis()
-            )
-            sixieme_ligne_colonne_2 = (
-                ConstructeurDeTextItem()
-                .avec_numero_page(numero_page)
-                .avec_texte("fort de mots de passe")
-                .avec_bbox(BoundingBox(l=317.0, t=529.0, r=417.0, b=539.0))
-                .construis()
-            )
-            legende_tableau = (
-                ConstructeurDeTextItem()
-                .avec_numero_page(numero_page)
-                .avec_texte(
-                    "TABLE 1 – Récapitulatif des menaces et contre-mesures sur les mots de passe"
-                )
-                .avec_bbox(BoundingBox(l=123.0, t=476.0, r=484.0, b=486.0))
-                .construis()
-            )
-
-            return ConversionResult(
-                document=DoclingDocument(
-                    name="",
-                    texts=[
-                        deuxieme_paragraphe,
-                        premiere_ligne_colonne_1,
-                        deuxieme_ligne_colonne_2,
-                        premiere_ligne_colonne_2,
-                        premier_paragraphe,
-                        troisieme_ligne_colonne_2,
-                        deuxieme_ligne_colonne_1,
-                        quatrieme_ligne_colonne_2,
-                        deuxieme_entete,
-                        cinquieme_ligne_colonne_2,
-                        premiere_entete,
-                        sixieme_ligne_colonne_2,
-                        legende_tableau,
-                    ],
-                ),
-                input=InputDocument(
-                    format=InputFormat.PDF,
-                    backend=PdfDocumentBackend,  # type: ignore[type-abstract]
-                    path_or_stream=Path(),
-                ),
-            )
-
-    def _convertisseur() -> Type[DocumentConverter]:
-        return ConverterDeTestAvecBbox
-
-    return _convertisseur
-
-
-@pytest.fixture
 def un_convertisseur_avec_un_titre_et_un_texte() -> Callable[
     [], Type[DocumentConverter]
 ]:
@@ -525,59 +359,6 @@ def un_convertisseur_avec_un_titre_et_un_texte() -> Callable[
         return ConverterDeTestAvecContenuSimple
 
     return _convertisseur
-
-
-class ConstructeurDeBaseChunk:
-    def __init__(self):
-        super().__init__()
-        self.texte = "Un texte"
-        self.bounding_box = BoundingBox(l=100.0, t=200.0, r=300.0, b=400.0)
-        self.numero_page = 1
-
-    def a_la_page(self, numero_page: int):
-        self.numero_page = numero_page
-        return self
-
-    def a_la_position(self, position: Position):
-        self.bounding_box = BoundingBox(
-            l=position.x,
-            t=position.y,
-            r=position.largeur + position.x,
-            b=position.hauteur + position.y,
-        )
-        return self
-
-    def avec_le_texte(self, texte: str):
-        self.texte = texte
-        return self
-
-    def construis(self) -> BaseChunk:
-        return BaseChunk(
-            text=self.texte,
-            meta=DocMeta(
-                doc_items=[
-                    DocItem(
-                        prov=[
-                            ProvenanceItem(
-                                page_no=self.numero_page,
-                                bbox=self.bounding_box,
-                                charspan=(0, 0),
-                            ),
-                        ],
-                        label=DocItemLabel.TEXT,
-                        self_ref="#/tables/9",
-                    )
-                ]
-            ),
-        )
-
-
-@pytest.fixture
-def un_constructeur_de_base_chunk() -> Callable[[], ConstructeurDeBaseChunk]:
-    def _constructeur_de_base_chunk() -> ConstructeurDeBaseChunk:
-        return ConstructeurDeBaseChunk()
-
-    return _constructeur_de_base_chunk
 
 
 @pytest.fixture
