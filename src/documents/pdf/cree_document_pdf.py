dfc939df789cad96b2b5eb1e132bc765a5163954
Bertrand Bougon <bertrand.bougon@gmail.com>
2026-03-04 08:58:44 +0100

    [REFACTO] Affecte un type PDF ou HTML aux documents distants à ajouter dans notre collection

--- src/documents/pdf/cree_document_pdf.py
+++ src/documents/pdf/cree_document_pdf.py
@@ -1,11 +1,21 @@
 import json
 import unicodedata
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Literal
 from urllib.parse import quote
 
 from configuration import MSC
 from documents.pdf.document_pdf import DocumentPDF
+
+
+@dataclass
+class URLDocument:
+    type: Literal["PDF", "HTML"]
+    url: str
+
+
+type DocumentDistant = dict[str, URLDocument]
 
 
 def cree_document_pdf(
@@ -22,6 +32,15 @@ def cree_document_pdf(
     if fichier_urls_specifiques:
         chemin_json = Path(fichier_urls_specifiques)
         if chemin_json.exists():
-            contenu = json.loads(chemin_json.read_text(encoding="utf-8"))
-            url = contenu.get(nom_fichier)
+
+            def as_url_document(d: dict) -> URLDocument | dict:
+                if "url" in d and "type" in d:
+                    return URLDocument(**d)
+                return d
+
+            contenu: DocumentDistant = json.loads(
+                chemin_json.read_text(encoding="utf-8"),
+                object_hook=as_url_document,
+            )
+            url = contenu[nom_fichier].url
     return DocumentPDF(chemin, url)
