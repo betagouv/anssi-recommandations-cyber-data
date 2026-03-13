5dccfc9bd305c13588299d3a5b0ffaa4eb3eec75
Bertrand Bougon <bertrand.bougon@gmail.com>
2026-03-13 12:00:01 +0100

    Ajoute les résultats des cas de tests dans le résultat final

--- src/evaluation/evaluateur_reformulation.py
+++ src/evaluation/evaluateur_reformulation.py
@@ -10,7 +10,9 @@ from evaluation.reformulation.evaluation import (
     QuestionAEvaluer,
     ResultatEvaluation,
 )
-from infra.evaluateur.deep_eval.evaluateur_deepeval_multi_processus import EvaluateurDeepevalMultiProcessus
+from infra.evaluateur.deep_eval.evaluateur_deepeval_multi_processus import (
+    EvaluateurDeepevalMultiProcessus,
+)
 
 
 def lance_evaluation(
@@ -18,7 +20,9 @@ def lance_evaluation(
     prompt: str,
     questions: list[QuestionAEvaluer],
 ) -> list[ResultatEvaluation]:
-    return EvaluateurReformulation(client_albert, prompt, EvaluateurDeepevalMultiProcessus()).evalue(questions=questions)
+    return EvaluateurReformulation(
+        client_albert, prompt, EvaluateurDeepevalMultiProcessus()
+    ).evalue(questions=questions)
 
 
 if __name__ == "__main__":
--- src/evaluation/reformulation/evaluation.py
+++ src/evaluation/reformulation/evaluation.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+from itertools import chain
 
+from deepeval.evaluate.types import TestResult
 from deepeval.test_case import LLMTestCase
 
 from adaptateurs.clients_albert import ClientAlbertReformulation
@@ -14,6 +16,7 @@ class ResultatEvaluation:
     question: str
     reformulation_ideale: str
     question_reformulee: str
+    resultats: list[TestResult]
 
 
 @dataclass
@@ -35,12 +38,6 @@ class EvaluateurReformulation:
         self._evaluateur = evaluateur
 
     def evalue(self, questions: list[QuestionAEvaluer]) -> list[ResultatEvaluation]:
-        self._evaluateur.evaluate(
-            test_cases=[
-                LLMTestCase(input="Question test", actual_output="Question reformulée")
-            ],
-            metrics=[MetriqueSuppressionParasites()],
-        )
         return [
             ResultatEvaluation(
                 question=question.question,
@@ -48,6 +45,26 @@ class EvaluateurReformulation:
                 question_reformulee=self._client_albert.reformule_la_question(
                     self._prompt, question.question
                 ),
+                resultats=self._execute_les_cas_de_test(question),
             )
             for question in questions
         ]
+
+    def _execute_les_cas_de_test(self, question: QuestionAEvaluer) -> list[TestResult]:
+        resultats_evaluation = self._evaluateur.evaluate(
+            test_cases=[
+                LLMTestCase(
+                    input=question.question,
+                    actual_output=self._client_albert.reformule_la_question(
+                        self._prompt, question.question
+                    ),
+                    expected_output=question.reformulation_ideale,
+                )
+            ],
+            metrics=[MetriqueSuppressionParasites()],
+        )
+        return list(
+            chain.from_iterable(
+                map(lambda resultats: resultats.test_results, resultats_evaluation)
+            )
+        )
