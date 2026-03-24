d4f3619cc8fd6f787f7c7bd868c0e1f9f9c2e43e
Bertrand Bougon <bertrand.bougon@gmail.com>
2026-03-24 16:00:12 +0100

    Initie la génération des questions

---
+++ src/jeopardy/client_albert_jeopardy.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass
+class RequeteCreationDocumentAlbert:
+    name: str
+    metadata: dict[str, Any]
+
+
+@dataclass
+class ReponseCreationCollection:
+    id: str
+
+
+class ClientAlbertJeopardy(ABC):
+    @abstractmethod
+    def cree_collection(self) -> ReponseCreationCollection:
+        pass
+
+    @abstractmethod
+    def ajoute_document(
+        self, identifiant_collection: str, document: RequeteCreationDocumentAlbert
+    ):
+        pass
