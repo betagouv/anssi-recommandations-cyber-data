9f25a273f1534338422978bf04d4393720fa79d4
Bertrand Bougon <bertrand.bougon@gmail.com>
2026-06-17 11:46:22 +0200

    Ajoute un bouton d’identification de test

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,10 +1,45 @@
 <script lang="ts">
-  console.log("Hello world");
+
+  import {startAuthentication} from "@simplewebauthn/browser";
+
+  type Initialisation = {
+    challenge: string;
+    id: string;
+  }
+
+  const authentifie = async () => {
+    console.log("authentifie");
+
+    const reponse = await fetch("/auth/initie", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({utilisateur: "bertrand.bougon"}),
+    });
+
+    const initialisation: Initialisation = await reponse.json();
+
+    const authenticationResponse =
+            await startAuthentication({optionsJSON: {challenge: initialisation.challenge, rpId: window.location.hostname, userVerification: "required", allowCredentials: [
+                  {id: initialisation.id, type: "public-key"}
+                ] }});
+
+    const reponseFinalisation = await fetch("/auth/finalise", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({credential: authenticationResponse, challenge: initialisation.challenge})
+    });
+
+    console.log(`CONNECTÉ ? ${reponseFinalisation.status} : ${await reponseFinalisation.text()} - ${await reponseFinalisation.json()}`);
+  }
 </script>
 
 
 <main class="main">
-  Hello world
+  <input type="button" value="bertrand.bougon" onclick={authentifie}>
 </main>
 
 <style lang="scss">
